75ce8840f8e16d0e3fbb0fa08ae9d57eb9ec7646
AlexxMotta <alexmota@alu.ufc.br>
2023-05-07 22:30:49 -0300

    Controller da FallingPlataform

---
+++ FallingPlataformController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlataformController : MonoBehaviour
+{
+    public float fallTime;
+    private TargetJoint2D target;
+    private BoxCollider boxCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GetComponent<TargetJoint2D>();
+        boxCollider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Invoke("Falling", fallTime);
+        }
+    }
+
+    void Falling()
+    {
+        target.enabled = false;
+        boxCollider.isTrigger = true;
+    }
+}
