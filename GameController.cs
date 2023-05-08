c0834274611f5cce59ef3349f5ace637a21b9381
AlexxMotta <alexmota@alu.ufc.br>
2023-05-07 22:31:10 -0300

    Controller do GameController para info gerais

---
+++ GameController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public int totalScore;
+    //importe UnityEngine.UI pra usar o scoreText
+    public Text scoreText;
+
+    public static GameController instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    public void UpdateScoreText()
+    {
+        scoreText.text = totalScore.ToString();
+    }
+
+}
