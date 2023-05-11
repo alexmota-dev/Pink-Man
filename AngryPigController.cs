a39a35e879048ac26db8c15acc446bd656588464
AlexxMotta <alexmota@alu.ufc.br>
2023-05-11 14:16:26 -0300

    Criando movimentando do enemy

--- AngryPigController.cs
+++ AngryPigController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class AngryPigController : MonoBehaviour
 {
+    public float velocidade;
+    public float distancia;
+    private float posicaoInicial;
+    private bool indoParaDireita = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        posicaoInicial = transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (indoParaDireita) {
+            transform.position += Vector3.right * velocidade * Time.deltaTime;
+        } else {
+            transform.position -= Vector3.right * velocidade * Time.deltaTime;
+        }
+
+        if (Mathf.Abs(transform.position.x - posicaoInicial) >= distancia) {
+            indoParaDireita = !indoParaDireita;
+        }
     }
 }
