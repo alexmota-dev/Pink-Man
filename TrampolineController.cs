cafd8a6f288136b16cb450b3d09c87560c0c16fb
Alex Mota <alexmota@alu.ufc.br>
2023-05-15 20:53:02 -0300

    adicionando animaçao ao Trampoline

--- TrampolineController.cs
+++ TrampolineController.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class TrampolineController : MonoBehaviour
 {
     public float JumpForce;
+    private Animator anim;
 
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }  
+    
     void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+            anim.SetTrigger("jump");
         }
     }
 }
