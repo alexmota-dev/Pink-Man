c250af2bddd6fb47f6ce78816007b3a506632076
Alex Mota <alexmota@alu.ufc.br>
2023-05-15 20:52:43 -0300

    criando script da serra SawController

---
+++ SawController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawController : MonoBehaviour
+{
+    public float speed;
+    public float distance;
+    private float initialPosition;
+    private bool walkingToTheRight = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovimentRightLeft(speed, distance, initialPosition);
+        // MovimentRightLeft();
+    }
+
+    private void MovimentRightLeft(float speed, float distance, float initialPosition)
+    {
+        if (walkingToTheRight) {
+            transform.eulerAngles = new Vector3(0f,180f,0f);
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        } else {
+            transform.eulerAngles = new Vector3(0f,0f,0f);
+            transform.position -= Vector3.right * speed * Time.deltaTime;
+        }
+        Debug.Log(transform.position.x);
+        if (Mathf.Abs(transform.position.x - initialPosition) >= distance) {
+            walkingToTheRight = !walkingToTheRight;
+        }
+    }
+}
