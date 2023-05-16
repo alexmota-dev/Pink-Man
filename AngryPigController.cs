c3a84e2730c0500143e64103163f81a3e2bb43a7
Alex Mota <alexmota@alu.ufc.br>
2023-05-16 18:34:09 -0300

    refatorando o AngryPig para que ele seja semelhante ao Goomba

--- AngryPigController.cs
+++ AngryPigController.cs
@@ -6,6 +6,7 @@ public class AngryPigController : MonoBehaviour
 {
     public float speed;
     public float distance;
+
     private float initialPosition;
     private bool walkingToTheRight = true;
     // Start is called before the first frame update
@@ -30,9 +31,30 @@ public class AngryPigController : MonoBehaviour
             transform.eulerAngles = new Vector3(0f,0f,0f);
             transform.position -= Vector3.right * speed * Time.deltaTime;
         }
-        Debug.Log(transform.position.x);
+        // Debug.Log(transform.position.x);
         if (Mathf.Abs(transform.position.x - initialPosition) >= distance) {
             walkingToTheRight = !walkingToTheRight;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            // Verifica a posição relativa do AngryPig em relação ao player
+            Vector2 contactPoint = collision.contacts[0].point;
+            Vector2 playerPosition = collision.gameObject.transform.position;
+
+            if (contactPoint.y < playerPosition.y)
+            {
+                // Player pulou no topo do AngryPig, então o AngryPig é destruído
+                Destroy(gameObject);
+            }
+            else
+            {
+                // Player encostou nas laterais do AngryPig, então o player é destruído
+                GameController.instance.PlayerDies(collision.gameObject);
+            }
         }
     }
 }
