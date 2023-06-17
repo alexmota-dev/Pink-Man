4cff063b0f5b8523168e912a92a15994922d8caa
Alex Mota <alexmota@alu.ufc.br>
2023-06-16 23:52:34 -0300

    Diminuido a complex do codigo movendo o if. Iniciando codigo do Bee

--- EnemyHeadController.cs
+++ EnemyHeadController.cs
@@ -7,7 +7,6 @@ public class EnemyHeadController : MonoBehaviour
     private int lives=2;
     private bool immunity = false;
     public string enemyName;
-
     IEnumerator WaitImmunity()
     {
         immunity = true;
@@ -17,36 +16,40 @@ public class EnemyHeadController : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        switch (enemyName)
+        if(collision.gameObject.tag == "Player")
         {
-            case "Pig":
-                if(collision.gameObject.tag == "Player")
-                {
-                    if(lives == 1)
-                    {
-                        if(!immunity)
+            switch (enemyName)
+            {
+                case "Pig":
+                        if(lives == 1)
                         {
-                            AngryPigController.instance.anim.SetBool("destroy",true);
-                            GameController.instance.ImpulseUp(11f, collision.gameObject.GetComponent<Rigidbody2D>());
-                            Destroy(transform.parent.gameObject, 0.45f);
+                            if(!immunity)
+                            {
+                                AngryPigController.instance.anim.SetBool("destroy",true);
+                                GameController.instance.ImpulseUp(11f, collision.gameObject.GetComponent<Rigidbody2D>());
+                                Destroy(transform.parent.gameObject, 0.45f);
+                            }
                         }
-                    }
-                    if(lives > 1)
-                    {
-                        AngryPigController.instance.anim.SetBool("hitOne",true);
-                        GameController.instance.ImpulseUp(11f, collision.gameObject.GetComponent<Rigidbody2D>());
-                        // AngryPigController.instance.ExpectToGetAngry();
-                        AngryPigController.instance.DoubleSpeed();
-                        AngryPigController.instance.anim.SetBool("isAngry",true);
-                        StartCoroutine(WaitImmunity());
-                        lives--;
-                    }
-                }
-                break;
-            case "Bee":
-                break;
-            default:
-                break;
+                        if(lives > 1)
+                        {
+                            AngryPigController.instance.anim.SetBool("hitOne",true);
+                            GameController.instance.ImpulseUp(11f, collision.gameObject.GetComponent<Rigidbody2D>());
+                            // AngryPigController.instance.ExpectToGetAngry();
+                            AngryPigController.instance.DoubleSpeed();
+                            AngryPigController.instance.anim.SetBool("isAngry",true);
+                            StartCoroutine(WaitImmunity());
+                            lives--;
+                        }
+                    break;
+                case "Bee":
+                    // BeeController.instance.anim.SetBool("destroy", true);
+                    transform.parent.gameObject.anim.SetBool("destroy", true);
+                    GameController.instance.ImpulseUp(11f, collision.gameObject.GetComponent<Rigidbody2D>());
+                    Destroy(transform.parent.gameObject, 0.5f);
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
