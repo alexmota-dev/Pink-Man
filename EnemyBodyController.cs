c384d6e867c259f869ae38da6d9394759ac6aab8
Alex Mota <alexmota@alu.ufc.br>
2023-06-26 15:50:29 -0300

    refatorando controllers do head e body dos enemys para funcionarem de forma inteligente

--- EnemyBodyController.cs
+++ EnemyBodyController.cs
@@ -1,14 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class EnemyBodyController : MonoBehaviour
 {
+    private int livesPig = 2;
+    private bool immunity = false;
+    public string enemyName;
+    //alterei o nome pra ficar o o body já que o tipo dela é diferente, o nome precisa ser corrigido posteriormente.
+    public event Action<EnemyBodyController> OnCollisionWithBullet;
+
+    public GameObject pineapplePrefab;
+
+    IEnumerator WaitImmunity()
+    {
+        immunity = true;
+        yield return new WaitForSeconds(2f);
+        immunity = false;
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
-            GameController.instance.PlayerDies(collision.gameObject);
+            GameController.instance.PlayerSuffersDamage(collision.gameObject);
+        }
+        if (collision.gameObject.tag == "BulletPlayer")
+        {
+            switch (enemyName)
+            {
+                case "Pig":
+                    if (livesPig == 1)
+                    {
+                        if (!immunity)
+                        {
+                            AngryPigController.instance.anim.SetBool("destroy", true);
+                            GameController.instance.ImpulseUp(11f, collision.gameObject.GetComponent<Rigidbody2D>());
+                            Destroy(transform.parent.gameObject, 0.45f);
+                        }
+                    }
+                    if (livesPig > 1)
+                    {
+                        AngryPigController.instance.anim.SetBool("hitOne", true);
+                        GameController.instance.ImpulseUp(11f, collision.gameObject.GetComponent<Rigidbody2D>());
+                        // AngryPigController.instance.ExpectToGetAngry();
+                        AngryPigController.instance.DoubleSpeed();
+                        AngryPigController.instance.anim.SetBool("isAngry", true);
+                        StartCoroutine(WaitImmunity());
+                        livesPig--;
+                    }
+                    break;
+                case "Bee":
+                    OnCollisionWithBullet?.Invoke(this);
+                    break;
+                case "Plant":
+                    pineapplePrefab.SetActive(true);
+                    OnCollisionWithBullet?.Invoke(this);
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
--- EnemyHeadController.cs
+++ EnemyHeadController.cs
@@ -6,10 +6,12 @@ using System;
 
 public class EnemyHeadController : MonoBehaviour
 {
-    private int lives=2;
+    private int lives = 2;
     private bool immunity = false;
     public string enemyName;
     public event Action<EnemyHeadController> OnCollisionWithPlayer;
+
+    public GameObject pineapplePrefab;
 
     IEnumerator WaitImmunity()
     {
@@ -20,32 +22,36 @@ public class EnemyHeadController : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "BulletPlayer")
         {
             switch (enemyName)
             {
                 case "Pig":
-                        if(lives == 1)
+                    if (lives == 1)
+                    {
+                        if (!immunity)
                         {
-                            if(!immunity)
-                            {
-                                AngryPigController.instance.anim.SetBool("destroy",true);
-                                GameController.instance.ImpulseUp(11f, collision.gameObject.GetComponent<Rigidbody2D>());
-                                Destroy(transform.parent.gameObject, 0.45f);
-                            }
-                        }
-                        if(lives > 1)
-                        {
-                            AngryPigController.instance.anim.SetBool("hitOne",true);
+                            AngryPigController.instance.anim.SetBool("destroy", true);
                             GameController.instance.ImpulseUp(11f, collision.gameObject.GetComponent<Rigidbody2D>());
-                            // AngryPigController.instance.ExpectToGetAngry();
-                            AngryPigController.instance.DoubleSpeed();
-                            AngryPigController.instance.anim.SetBool("isAngry",true);
-                            StartCoroutine(WaitImmunity());
-                            lives--;
+                            Destroy(transform.parent.gameObject, 0.45f);
                         }
+                    }
+                    if (lives > 1)
+                    {
+                        AngryPigController.instance.anim.SetBool("hitOne", true);
+                        GameController.instance.ImpulseUp(11f, collision.gameObject.GetComponent<Rigidbody2D>());
+                        // AngryPigController.instance.ExpectToGetAngry();
+                        AngryPigController.instance.DoubleSpeed();
+                        AngryPigController.instance.anim.SetBool("isAngry", true);
+                        StartCoroutine(WaitImmunity());
+                        lives--;
+                    }
                     break;
                 case "Bee":
+                    OnCollisionWithPlayer?.Invoke(this);
+                    break;
+                case "Plant":
+                    pineapplePrefab.SetActive(true);
                     OnCollisionWithPlayer?.Invoke(this);
                     break;
                 default:
