1f4098d97161a75d75b28b77aa4aacd597000f9a
Alex Mota <alexmota@alu.ufc.br>
2023-06-26 15:39:52 -0300

    refatorando player controller e adicionando novas funcoes

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
@@ -16,16 +17,41 @@ public class PlayerController : MonoBehaviour
     public Animator anim;
     private float speedY;
     public static PlayerController instance;
+
+    private int numberOfShots = 0;
+    private bool shotSkillIsActive = false;
+    public GameObject bulletPrefab;
+    public float bulletSpeed;
+    public Transform shootingPoint;
+
+    public int lifes = 4;
+
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         instance = this;
+        //revisar event Action<T>
+        //revisar essa parte do FindObjectsOfType
+        EnemyHeadController[] enemyHeadControllers = FindObjectsOfType<EnemyHeadController>();
+        foreach (EnemyHeadController enemyController in enemyHeadControllers)
+        {
+            //revisar essa parte da chamada função
+            enemyController.OnCollisionWithPlayer += DestroyEnemy;
+        }
+
+        EnemyBodyController[] enemyBodyControllers = FindObjectsOfType<EnemyBodyController>();
+        foreach (EnemyBodyController enemyController in enemyBodyControllers)
+        {
+            //revisar essa parte da chamada função
+            enemyController.OnCollisionWithBullet += DestroyEnemy;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        ShotSkill();
         fallAndUpAnimationUpdate();
         Move();
         Jump();
@@ -41,36 +67,36 @@ public class PlayerController : MonoBehaviour
     {
         if (speedY > 0)
         {
-            anim.SetBool("up",true);
+            anim.SetBool("up", true);
         }
         else if (speedY < 0)
         {
-            anim.SetBool("fall",true);
+            anim.SetBool("fall", true);
         }
-        else if(speedY == 0)
+        else if (speedY == 0)
         {
-            anim.SetBool("fall",false);
-            anim.SetBool("up",false);
+            anim.SetBool("fall", false);
+            anim.SetBool("up", false);
         }
     }
-    
+
     private void walkAnimationUpdate(float inputGetAxis)
     {
-        if(inputGetAxis > 0f)
+        if (inputGetAxis > 0f)
         {
             //andando pra direita
-            anim.SetBool("walk",true);
-            transform.eulerAngles = new Vector3(0f,0f,0f);
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
         }
-        if(inputGetAxis < 0f)
+        if (inputGetAxis < 0f)
         {
             //andando pra direita
-            anim.SetBool("walk",true);
-            transform.eulerAngles = new Vector3(0f,180f,0f);
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
         }
-        if(inputGetAxis == 0)
+        if (inputGetAxis == 0)
         {
-            anim.SetBool("walk",false);
+            anim.SetBool("walk", false);
         }
     }
 
@@ -82,17 +108,19 @@ public class PlayerController : MonoBehaviour
         rig.velocity = new Vector2(movement * speed, rig.velocity.y);
         walkAnimationUpdate(movement);
     }
+
     void Jump()
     {
-        if(Input.GetButtonDown("Jump") && !isOnFan)
+        if (Input.GetButtonDown("Jump") && !isOnFan)
         {
-            if(!isJumping)
+            if (!isJumping)
             {
                 GameController.instance.ImpulseUp(JumpForce, rig);
                 doubleJump = true;
             }
-            else{
-                if(doubleJump)
+            else
+            {
+                if (doubleJump)
                 {
                     GameController.instance.ImpulseUp(JumpForce, rig);
                     doubleJump = false;
@@ -101,15 +129,24 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    void ShotSkill()
+    {
+        if (Input.GetButtonDown("Fire1") && shotSkillIsActive && numberOfShots > 0)
+        {
+            CreateBullet();
+            numberOfShots--;
+        }
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Spike" || collision.gameObject.tag == "Bullet")
+        if (collision.gameObject.tag == "Spike" || collision.gameObject.tag == "Bullet")
         {
-            GameController.instance.PlayerDies(gameObject);
+            GameController.instance.PlayerSuffersDamage(gameObject);
         }
 
 
-        if(collision.gameObject.layer == 8)
+        if (collision.gameObject.layer == 8)
         {
             isJumping = false;
         }
@@ -117,24 +154,24 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if(collision.gameObject.layer == 8)
+        if (collision.gameObject.layer == 8)
         {
             isJumping = false;
         }
     }
 
- 
+
     void OnCollisionExit2D(Collision2D collision)
     {
-        if(collision.gameObject.layer == 8)
+        if (collision.gameObject.layer == 8)
         {
-           isJumping = true; 
+            isJumping = true;
         }
     }
 
     void OnTriggerStay2D(Collider2D collider)
     {
-        if(collider.gameObject.layer == 11)
+        if (collider.gameObject.layer == 11)
         {
             isOnFan = true;
         }
@@ -142,9 +179,55 @@ public class PlayerController : MonoBehaviour
 
     void OnTriggerExit2D(Collider2D collider)
     {
-        if(collider.gameObject.layer == 11)
+        if (collider.gameObject.layer == 11)
         {
             isOnFan = false;
+        }
+    }
+
+    private void DestroyEnemy(EnemyHeadController enemy)
+    {
+        Animator enemyAnimator = enemy.transform.parent.GetComponent<Animator>();
+        if (enemyAnimator != null)
+        {
+            enemyAnimator.SetBool("destroy", true);
+        }
+        GameController.instance.ImpulseUp(11f, gameObject.GetComponent<Rigidbody2D>());
+        Destroy(enemy.gameObject.transform.parent.gameObject, 0.5f);
+        // bee.gameObject.transform.parent.gameObject.anim.SetBool("destroy", true);
+    }
+
+    private void DestroyEnemy(EnemyBodyController enemy)
+    {
+        Animator enemyAnimator = enemy.transform.parent.GetComponent<Animator>();
+        if (enemyAnimator != null)
+        {
+            enemyAnimator.SetBool("destroy", true);
+        }
+        GameController.instance.ImpulseUp(11f, gameObject.GetComponent<Rigidbody2D>());
+        Destroy(enemy.gameObject.transform.parent.gameObject, 0.5f);
+        // bee.gameObject.transform.parent.gameObject.anim.SetBool("destroy", true);
+    }
+
+    public void setShotSkillIsActive(bool value)
+    {
+        shotSkillIsActive = value;
+        numberOfShots += 2;
+    }
+
+    void CreateBullet()
+    {
+        // Instancia a bala
+        GameObject bullet = Instantiate(bulletPrefab, shootingPoint.position, Quaternion.identity);
+        // Adiciona velocidade à bala
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        if (gameObject.transform.rotation.eulerAngles.y == 0f)
+        {
+            rb.velocity = Vector2.right * bulletSpeed;
+        }
+        if (gameObject.transform.rotation.eulerAngles.y == 180f)
+        {
+            rb.velocity = Vector2.left * bulletSpeed;
         }
     }
 }
