7a076411af26277c91da6d1dea33ee52af14a643
Alex Mota <alexmota@alu.ufc.br>
2023-06-26 15:43:02 -0300

    criando PineapleController para o abacaxi e a logica de shots do player

---
+++ PineapleController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PineapleController : MonoBehaviour
+{
+    private SpriteRenderer spriteRender;
+    private CircleCollider2D circleCollider;
+
+    public GameObject collected;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRender = GetComponent<SpriteRenderer>();
+        circleCollider = GetComponent<CircleCollider2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            PlayerController.instance.setShotSkillIsActive(true);
+            //desativando o spriteRenderer
+            spriteRender.enabled = false;
+            //desativando o circleCollider
+            circleCollider.enabled = false;
+            //ativando o gameObject collected
+            collected.SetActive(true);
+
+            //player ganha duas balas pra atirar
+            //logica pra ele ganhar balas
+
+            Destroy(gameObject, 0.25f);
+        }
+    }
+}
